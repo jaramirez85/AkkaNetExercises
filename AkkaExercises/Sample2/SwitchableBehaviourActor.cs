9b4827e0d24d94a103985992f35e9f928c2ccaa0
jaramirez85 <jaramirez85@gmail.com>
2017-09-05 21:07:12 -0500

    SwitchableBehaviour sample

---
+++ AkkaExercises/Sample2/SwitchableBehaviourActor.cs
@@ -0,0 +1,29 @@
+﻿using Akka.Actor;
+using System;
+
+namespace AkkaExercises.Sample2
+{
+    class SwitchableBehaviourActor : ReceiveActor
+    {
+        public SwitchableBehaviourActor()
+        {
+            Become(ABehaviour);
+        }
+
+        private void ABehaviour()
+        {
+            Receive<string>(_ => {
+                Console.WriteLine("From (A) Behaviour");
+                Become(BBehaviour);
+            });
+        }
+
+        private void BBehaviour()
+        {
+            Receive<string>(_ => {
+                Console.WriteLine("From (B) Behaviour");
+                Become(ABehaviour);
+            });
+        }
+    }
+}
---
+++ AkkaExercises/Sample2/SwitchableBehaviourApp.cs
@@ -0,0 +1,19 @@
+﻿using Akka.Actor;
+using System;
+
+namespace AkkaExercises.Sample2
+{
+    class SwitchableBehaviourApp
+    {
+        static void Main(string[] args)
+        {
+            var system = ActorSystem.Create("MySystemBehavior");
+            var behaviour = system.ActorOf<SwitchableBehaviourActor>("behaviour");
+            behaviour.Tell("hi1");
+            behaviour.Tell("hi2");
+            behaviour.Tell("hi3");
+            Console.ReadKey();
+            system.Terminate();
+        }
+    }
+}
